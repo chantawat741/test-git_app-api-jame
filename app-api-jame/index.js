033d551a9a318d403b8b8b623a123ff5e4a7278b
DevJame <chantawat741@gmail.com>
2021-03-16 11:40:27 +0700

    first commit

---
+++ app-api-jame/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+app.get("/", (req, res) => {
+    res.json({ message: "Ahoy!" })
+})
+
+app.get("/data", (req, res) => {
+    // ? SHOW
+    res.json({ message: "get DATA" })
+})
+
+app.post("/", (req, res) => {
+    // ? POST
+    res.json({ message: "create DATA" })
+})
+
+app.put("/", (req, res) => {
+    // ! UPDATE
+    res.json({ message: "updated DATA" })
+})
+
+app.delete("/", (req, res) => {
+    // ! DEALETE
+    res.json({ message: "delete DATA" })
+})
+
+app.listen(9000, () => {
+    console.log("Application is running on port 9000")
+})
